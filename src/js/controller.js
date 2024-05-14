8a4bd7df2fc291e9b17aa6eca4966d4b915dcb35
Sohail025 <msohail0264@gmail.com>
2024-05-14 13:16:24 +0500

    last commit

---
+++ src/js/controller.js
@@ -0,0 +1,70 @@
+import * as modal from './modal.js';
+import Recipeview from './views/view.js';
+import Searchview from './views/searchView.js';
+import Resultsview from './views/Resultsview.js';
+import paginationView from './views/paginationView.js';
+import bookmarkview from './views/bookmarks.js';
+import addRecipeView from './views/addrecipe.js';
+import { async } from 'regenerator-runtime';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+let API = async () => {
+  try {
+    let id = window.location.hash.slice(1);
+    if (!id) return;
+    Recipeview.spinner();
+    await modal.fetchingData(id);
+    Recipeview.render(modal.state.recipe);
+    controlServings();
+  } catch (err) {
+    Recipeview.renderError();
+  }
+};
+const SearchFunc = async () => {
+  try {
+    let quary = Searchview.quaryGet();
+    if (!quary) return;
+    await modal.searchResults(quary);
+    Resultsview.render(modal.getSearchpage());
+    bookmarkview.render(modal.state.bookmark);
+    paginationView.render(modal.state.search);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const paginationbtn = pageGoTo => {
+  Resultsview.render(modal.getSearchpage(pageGoTo));
+  paginationView.render(modal.state.search);
+};
+const controlServings = newservings => {
+  modal.updateServings(newservings);
+  Recipeview.render(modal.state.recipe);
+};
+const bookmark = () => {
+  if (!modal.state.recipe.bookmarked) modal.bookmark(modal.state.recipe);
+  else modal.deletebookmarks(modal.state.recipe.id);
+  Recipeview.render(modal.state.recipe);
+  bookmarkview.render(modal.state.bookmark);
+};
+const updatebookmark = () => {
+  bookmarkview.render(modal.state.bookmark);
+};
+const addRecipeControler = data => {
+  try {
+    modal.uploadReciple(data);
+  } catch (error) {
+    console.log(error);
+    addRecipeView.renderError(error.message);
+  }
+};
+const init = () => {
+  bookmarkview.ControlBookmarks(updatebookmark);
+  Recipeview.hashchange(API);
+  Searchview.handlerFunc(SearchFunc);
+  paginationView.paginationHander(paginationbtn);
+  Recipeview.UpdateServingsHandler(controlServings);
+  Recipeview.bookmarkHandler(bookmark);
+  addRecipeView.Recipehandler(addRecipeControler);
+};
+init();
